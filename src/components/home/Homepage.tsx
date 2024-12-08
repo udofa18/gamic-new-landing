8a09f357954c17f06f848a9ee18b71ff37ee115c
udofa18 <danieludofa452@gmail.com>
2024-12-08 19:43:32 +0100

    dda

---
+++ src/components/home/Homepage.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Section1 from "@/components/home/Section1";
+import Section2 from "@/components/home/Section2";
+import Section3 from "@/components/home/Section3";
+import Section4 from "@/components/home/Section4";
+import Section5 from "@/components/home/Section5";
+import Section6 from "@/components/home/Section6";
+import Section7 from "@/components/home/Section7";
+
+const Homepage = () => {
+  return (
+    <div>
+      <Section1 />
+      <Section2 />
+      <Section3 />
+      <Section4 />
+      <Section5 />
+      <Section6 />
+      <Section7 />
+    </div>
+  );
+};
+
+export default Homepage;
