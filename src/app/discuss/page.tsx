f1b2695167521d28a46329daa5b1a1a026b1c976
udofa18 <danieludofa452@gmail.com>
2025-01-27 14:57:39 +0100

    `Removed policy component and updated privacy policy link in discuss page`

--- src/app/discuss/page.tsx
+++ src/app/discuss/page.tsx
@@ -131,7 +131,7 @@ const page = () => {
             <span className="flex item-center gap-2">
               <Checkbox checked={isChecked} onChange={handleCheckboxChange} />
               <p onClick={handleCheckboxChange} style={{ cursor: "pointer" }}>
-                You agree to our friendly privacy policy.
+                You agree to our friendly <a href="/policy" className="underline" target="_blank">privacy policy.</a>
               </p>
             </span>
             <span className="flex flex-col item-center gap-2">
